58b72d552dc4f8cdee33c48364ab3896f65a6043
sharon-ku <sharon-ku@users.noreply.github.com>
2021-01-21 00:10:45 -0500

    PF: added remainder of images; created html pages for best-practices and world-of-type; changed logo to white

---
+++ js/navFunctions.js
@@ -0,0 +1,13 @@
+// /* Open when someone clicks on the span element */
+// function openNav() {
+//   document.getElementById("table-of-contents").style.width = "100%";
+//   document.getElementById("table-of-contents").style.padding = "100px";
+//   document.getElementById("close-button").style.display = "flex";
+// }
+//
+// /* Close when someone clicks on the "x" symbol inside the overlay */
+// function closeNav() {
+//   document.getElementById("table-of-contents").style.width = "0%";
+//   document.getElementById("table-of-contents").style.padding = "0px";
+//   document.getElementById("close-button").style.display = "none";
+// }
